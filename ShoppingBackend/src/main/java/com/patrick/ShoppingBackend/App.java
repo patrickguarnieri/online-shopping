09b977560463b3f806df08f004bef25144197882
Patrick Guarnieri <patrick.guarnieri@hotmail.com>
2018-08-07 23:20:11 +0100

    Commit #1

---
+++ ShoppingBackend/src/main/java/com/patrick/ShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.patrick.ShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
